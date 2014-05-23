098732db2b3d053193ae0765ce99790264ba9d38
sscarduzio_mpme <scarduzio@gmail.com>
2014-05-23 15:29:30 +0100

    better tests

--- src/test/scala/eu/codesigner/finagle/ws2redis/Ws2redisAdapterTest.scala
+++ src/test/scala/eu/codesigner/finagle/ws2redis/Ws2redisAdapterTest.scala
@@ -12,18 +12,24 @@ class Ws2redisAdapterTest extends FlatSpec with Matchers {
   it should "recognize 2-arity commands " in {
     val (cmd, args) = Ws2redisAdapter.adaptRedisCommand("GET myKey")
     cmd should be("GET")
-    args should be(List("myKey".getBytes()))
+    args.map(new String(_)) should be(List("myKey"))
   }
 
   it should "recognize N-arity commands " in {
     val (cmd, args) = Ws2redisAdapter.adaptRedisCommand("ZRANGE myZset 0 -1")
         cmd should be("ZRANGE")
-        args should be(List("myZset".getBytes(),"0".getBytes(), "-1".getBytes() ))
+        args.map(new String(_)) should be(List("myZset", "0", "-1"))
   }
 
-  it should "throw Exception if an empty string is popped" in {
+  it should "throw Exception if an empty string is given" in {
     a[Exception] should be thrownBy {
       Ws2redisAdapter.adaptRedisCommand("")
+    }
+  }
+  
+  it should "throw Exception if an single space string is given" in {
+    a[Exception] should be thrownBy {
+      Ws2redisAdapter.adaptRedisCommand(" ")
     }
   }
 }
